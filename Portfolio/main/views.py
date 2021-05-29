9f2b754bc600a782a56c2ad07a5bfe951c0f11dd
kprakhar27 <kumarprkhr@gmail.com>
2021-05-29 17:43:15 +0530

    changes

---
+++ Portfolio/main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    context = {}
+    return render(request, 'main/index.html', context)
+
+
+def projects(request):
+    context = {}
+    return render(request, 'main/projects.html', context)
+
+
+def languages(request):
+    context = {}
+    return render(request, 'main/languages.html', context)
